eec58dd93132ea5c8cc3cfc1b16f50bc684ea750
blessedadudu <adududuke@gmail.com>
2024-05-10 15:33:03 +0100

    add env

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+require("dotenv").config();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
+  res.send("Hello World!" + " - " + process.env.MESSAGE);
+});
 
 app.get("/ping", (req, res) => {
-  res.send({ success: true })
-})
+  res.send({ success: true });
+});
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is listening on port 3000")
-})
+  console.log("Server is listening on port 3000");
+});
